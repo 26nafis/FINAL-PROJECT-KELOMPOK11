4bbafb9270b8f6c41d3ff358e9f9bb209907e350
Rakha-arch <miftahuzahran@gmail.com>
2026-09-01 02:05:52 +0700

    Menambahkan model OrderItem dengan kolom id, quantity, price, dan subtotal.

---
+++ backend/models/orderitem.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const OrderItem = sequelize.define(
+  'OrderItem',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL(15, 2),
+      allowNull: false
+    },
+
+    subtotal: {
+      type: DataTypes.DECIMAL(15, 2),
+      allowNull: false
+    }
+  },
+  {
+    tableName: 'order_items',
+    timestamps: true
+  }
+);
+
+module.exports = OrderItem;
